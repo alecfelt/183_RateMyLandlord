fd9b0c34f9e1eb37570b0935fed092e8d3a656ef
Alec Felt <allfelt@ucsc.edu>
2018-05-20 20:43:48 -0700

    tandom changes

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8ecdf5d90a56e43d$f34a0bb46a2f2198b1bdbe16d9e924712dac312d"
+password="pbkdf2(1000,20,sha512)$b57b61bc6ba1dfc1$edf8fba5aa4a4f5ab34faafc6a5f269c36cb8e88"
