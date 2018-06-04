8d6a8c30717aa97b8ec4b01d1b30d5c16027a494
Alec Felt <allfelt@ucsc.edu>
2018-06-03 18:05:51 -0700

    just trying to merge

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 password="pbkdf2(1000,20,sha512)$b57b61bc6ba1dfc1$edf8fba5aa4a4f5ab34faafc6a5f269c36cb8e88"
+=======
+password="pbkdf2(1000,20,sha512)$a31114b59bb6aede$14914cca7b57b3ac97e9e8d7f44d8b098e3ce816"
+>>>>>>> Stashed changes
