a19b2134634b7d9c4e9eb763861a1433cc8835d6
Alec Felt <allfelt@ucsc.edu>
2018-06-10 15:19:14 -0700

    added form validation for WriteReview

--- applications/rml/static/js/default_index.js
+++ applications/rml/static/js/default_index.js
@@ -44,6 +44,13 @@ Vue.component('WriteReview', {
       }
     },
     validate_review() {
+      for(var key in this._data) {
+        console.log(key);
+        console.log(this._data[key]);
+        // if(this._data[key] == null) {
+        //   return false;
+        // }
+      }
       return true;
     }
   },
