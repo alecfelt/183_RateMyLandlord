3ecc1d5962c06cf5110adae2c6ded89e064edbe3
Alec Felt <allfelt@ucsc.edu>
2018-06-11 22:11:59 -0700

    merge

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$aaa3ebe7e8249ae8$04a180c201a7a34b68ca01099c0bcacff0dd531a"
-=======
-password="pbkdf2(1000,20,sha512)$844dd6645381bf8d$c8f44aa8d5242e4332906f888c636a401985980c"
->>>>>>> f2d84d386ff2f64720d255a28df94a432011b92d
-=======
-password="pbkdf2(1000,20,sha512)$9db8e76ab6d7f310$d89ee144546a949db0e3fe01ab65438cdffb9e25"
->>>>>>> cc6354927855e4ad2343ad20baa6d86b06e67998
+password="pbkdf2(1000,20,sha512)$9bfa1a74b8eac3aa$e2795e038bfd6cb82cd27c005b2620ff1018c01f"
