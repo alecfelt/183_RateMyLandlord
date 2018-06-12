35e7e8117925fb7652e8430f24718ca2ae6641b0
Mary Chern <mary.chern10@gmail.com>
2018-06-12 01:31:37 -0700

    merge

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9bfa1a74b8eac3aa$e2795e038bfd6cb82cd27c005b2620ff1018c01f"
+password="pbkdf2(1000,20,sha512)$87589a666befa0ab$a2c6f37b553fb36ddd35a5cf5edea7d1bd73ad49"
